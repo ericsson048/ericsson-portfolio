2e20a1d59e7b816c058990ab0fb7419e607aac1a
Ericsson ISHAKA <123806773+ericsson048@users.noreply.github.com>
2026-01-04 14:32:37 +0200

    fix: Escape apostrophes in contact page for ESLint compliance

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -44,9 +44,9 @@ function ContactPage() {
           {/* Form Section */}
           <div className="xl:w-[54%] order-2 xl:order-none">
             <form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
-              <h3 className="text-4xl text-emerald-400">Let's work together</h3>
+              <h3 className="text-4xl text-emerald-400">Let&apos;s work together</h3>
               <p className="text-white/60">
-                Have a project in mind or just want to say hi? Fill out the form below and I'll get back to you as soon as possible. I'm always excited to discuss new opportunities and collaborate on interesting projects.
+                Have a project in mind or just want to say hi? Fill out the form below and I&apos;ll get back to you as soon as possible. I&apos;m always excited to discuss new opportunities and collaborate on interesting projects.
               </p>
 
               {/* Name Inputs */}
